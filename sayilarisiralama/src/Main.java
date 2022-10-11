3cace992326a3723190e912864054cc0c84cacce
ceereenkoc <111969447+ceereenkoc@users.noreply.github.com>
2022-10-11 21:13:34 +0300

    Sayıları Küçükten Büyüğe Sıralayan Program

---
+++ sayilarisiralama/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Birinci sayıyı giriniz: ");
+        a = input.nextInt();
+        System.out.print("İkinci sayıyı giriniz: ");
+        b = input.nextInt();
+        System.out.print("Üçünc sayıyı giriniz: ");
+        c = input.nextInt();
+
+        if((a > b) && (a >c)){
+            if(b>c)
+                System.out.print("c < b < a");
+            else
+                System.out.print("b < c < a");
+        }
+        else if((b>a) && (b>c)){
+            if(a>c)
+                System.out.print("c < a < b");
+            else
+                System.out.print("a < c < b");
+        }
+        else
+            if(a>b)
+                System.out.print("b < a < c");
+            else
+                System.out.print("a < b < c");
+    }
+}
